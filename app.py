7012fd400b55193d704e78d9b2b1833207d25e7b
ThaRising <kochbe.ber@gmail.com>
2020-02-09 23:23:05 +0100

    Added test query to main file

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from models import Schema
+import db
+import sqlite3
 
 app = Flask(__name__)
 
@@ -9,4 +12,14 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run()
+    data = sqlite3.connect(":memory:")
+    database = data.cursor()
+    database.execute("""
+        CREATE TABLE IF NOT EXISTS User (
+        id INTEGER PRIMARY KEY,
+        Name TEXT,
+        CreatedOn Date DEFAULT CURRENT_DATE )""")
+    database.execute("""INSERT INTO User VALUES ('7', 'Ben', '2')""")
+    database.execute("""SELECT * FROM User WHERE Name='Ben'""")
+    print(database.fetchall())
+    app.run(debug=True)
