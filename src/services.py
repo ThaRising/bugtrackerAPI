eeca9c76f1b72b0586d416e56b948dd8336a7f08
ThaRising <kochbe.ber@gmail.com>
2020-02-16 13:01:47 +0100

    Reworked service structure

--- src/services.py
+++ src/services.py
@@ -1,37 +1,112 @@
-from src.models import Tag, Type
+import src.models as models
 from src import db
 from werkzeug.wrappers import BaseResponse
+from typing import List
 
 
-class TagService:
-    def __init__(self):
-        self.model = Tag
+class Service:
+    def __init__(self, model: db.Model):
+        self.model: db.Model = model
 
-    def create(self, params: dict):
-        """ Creates a new Tag with data provided by the user and inserts it into the database """
-        create = self.model(**params)
-        db.session.add(create)
+    @property
+    def model(self):
+        return self._model
+
+    @model.setter
+    def model(self, model_):
+        if not isinstance(model_(), db.Model):
+            raise TypeError("Model must be a SQLAlchemy Model class.")
+        self._model = model_
+
+    @model.deleter
+    def model(self):
+        raise AttributeError("Cannot delete model attribute.")
+
+    def __repr__(self):
+        return f"'{self.__class__.__name__}'('{self.model.__class__.__name__}')'"
+
+    def __len__(self):
+        return len(self.model.query.all())
+
+    def __getitem__(self, item):
+        if type(item) == int:
+            return self.get_all()[item]
+        elif type(item) == str or type(item) == dict:
+            return self.get_by_attr(item)
+
+    def create(self, params: dict) -> db.Model:
+        created_object = self.model(**params)
+        db.session.add(created_object)
         db.session.commit()
-        return str(create)
+        return created_object
 
-    def get_all(self):
-        """ Returns all Tags """
-        query_results = str(self.model.query.all())
-        return BaseResponse("Not found", status=404) if query_results is None else query_results
+    def get_all(self) -> List[db.Model]:
+        return self.model.query.all()
 
-    def get_by_name(self, req_name: str) -> BaseResponse:
-        """ Searches for a Tag with a specific name key and returns the first result if any """
-        query_result = str(self.model.query.filter_by(name=req_name).first())
-        return BaseResponse("Not found", status=404) if query_result is None else query_result
+    def get_by_attr(self, key) -> db.Model:
+        raise NotImplementedError("get_by_attr method not defined.")
+
+    def update_by_attr(self, key, params: dict) -> db.Model:
+        raise NotImplementedError("update_by_attr method not defined.")
+
+    def delete_by_attr(self, key) -> None:
+        raise NotImplementedError("delete_by_attr method not defined.")
+
+    def backref_by_id(self, id_: int) -> db.Model:
+        raise NotImplementedError("backref_by_id method not defined.")
 
 
-class TypeService:
-    def __init__(self):
-        self.model = Type
+class GenericNameService(Service):
+    def get_by_attr(self, key: str) -> db.Model:
+        return self.model.query.filter_by(name=key).first()
 
-    def create(self, params: dict):
-        """ Creates a new Type with data provided by the user and inserts it into the database """
-        create = self.model(**params)
-        db.session.add(create)
+    def update_by_attr(self, key: str, params: dict) -> db.Model:
+        updated_model = self[key]
+        for field, value in params.items():
+            setattr(updated_model, field, value)
         db.session.commit()
-        return str(create)
+        return updated_model
+
+    def delete_by_attr(self, key: str) -> None:
+        db.session.delete(self[key])
+        db.session.commit()
+
+    def backref_by_id(self, id_: int) -> db.Model:
+        return self.model.query.filter_by(id=id_).first()
+
+
+class TagService(GenericNameService):
+    def __init__(self):
+        super(TagService, self).__init__(models.Tag)
+
+
+class TypeService(GenericNameService):
+    def __init__(self):
+        super(TypeService, self).__init__(models.Type)
+
+
+class UserService(GenericNameService):
+    def __init__(self):
+        super(UserService, self).__init__(models.User)
+
+
+class CommentService(Service):
+    def __init__(self):
+        super(CommentService, self).__init__(models.Comment)
+
+    def get_by_attr(self, key: dict) -> db.Model:
+        return self.model.query.filter_by(**key).first()
+
+    def update_by_attr(self, key: dict, params: dict) -> db.Model:
+        updated_model = self[key]
+        for field, value in params.items():
+            setattr(updated_model, field, value)
+        db.session.commit()
+        return updated_model
+
+    def delete_by_attr(self, key: dict) -> None:
+        db.session.delete(self[key])
+        db.session.commit()
+
+    def backref_by_id(self, composite: dict) -> db.Model:
+        return self.model.query.filter_by(**composite).first()
