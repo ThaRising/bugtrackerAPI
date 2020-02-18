deb4a0fca090acb881c60372e4b700bc8ed0c10f
ThaRising <kochbe.ber@gmail.com>
2020-02-18 21:44:04 +0100

    Fixed bug with foreign key validation

--- src/models.py
+++ src/models.py
@@ -48,7 +48,6 @@ class Type(db.Model):
     __tablename__ = "type"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False, unique=True)
-    issues = db.relationship("Issue", backref="type")
     __table_args__ = (
         db.CheckConstraint("length(name) > 0",
                            name="name_min_length"),
