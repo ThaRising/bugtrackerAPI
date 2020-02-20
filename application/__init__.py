907e4f850fc4c954ba9b223261766e87b8bca8c1
ThaRising <kochbe.ber@gmail.com>
2020-02-20 14:21:26 +0100

    Added additional dependencies required for testing

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+def create_app(testing: bool =False):
+    app = Flask(__name__)
+    
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
