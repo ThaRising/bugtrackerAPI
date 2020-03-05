38b1c5e5aa65e58672eb088779e47feb277b71ae
ThaRising <kochbe.ber@gmail.com>
2020-03-05 23:50:00 +0100

    Copied card models from Frontend for easier parsing

---
+++ application/models/data_models.py
@@ -0,0 +1,22 @@
+PRIORITY = [
+    ("TRIVIAL", "Trivial"),
+    ("MINOR", "Minor"),
+    ("NORMAL", "Normal"),
+    ("MAJOR", "Major"),
+    ("CRITICAL", "Critical")
+]
+
+STATUS = [
+    ("OPEN", "Open"),
+    ("PROGRESS", "Progress"),
+    ("REVIEW", "Review"),
+    ("TEST", "Test"),
+    ("DONE", "Done")
+]
+
+TYPE = [
+    ("BUG", "Bug"),
+    ("FEATURE", "Feature"),
+    ("MEETING", "Meeting"),
+    ("REFACTOR", "Refactor")
+]
