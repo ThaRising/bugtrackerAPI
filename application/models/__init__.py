cb3f4bd9ac3cdfb68dbf33286f0c6c9b7f7bc7e1
ThaRising <kochbe.ber@gmail.com>
2020-03-05 23:50:28 +0100

    Created public interface for models

---
+++ application/models/__init__.py
@@ -0,0 +1,4 @@
+from .models import Tag, Type, User, Comment, Issue
+from .data_models import PRIORITY, STATUS, TYPE
+
+__all__ = ["Tag", "Type", "User", "Comment", "Issue", "PRIORITY", "STATUS", "TYPE"]
