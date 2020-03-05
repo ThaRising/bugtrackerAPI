f3c9595b9ab92a2ed9c416f9ca66dc52f4266d86
ThaRising <kochbe.ber@gmail.com>
2020-03-05 22:41:06 +0100

    Finished documentation and reduced boilerplate code in refactor

--- application/views/tag.py
+++ application/views/tag.py
@@ -3,46 +3,25 @@ from flask_restplus import fields as flask_fields
 from webargs import fields, validate
 from webargs.flaskparser import use_kwargs
 from application.controllers import TagController
-from . import Collection
+from . import CollectionFactory, ItemFactory
 
 api = Namespace("tags", description="Tag Resource Endpoint")
-
 controller = TagController
 
-tag = api.model("Tag", {
+rest_model = api.model("Tag", {
     "id": flask_fields.Integer(),
     "name": flask_fields.String(),
     "background": flask_fields.String(),
     "color": flask_fields.String()
 })
 
+collection = CollectionFactory(controller, rest_model)
+item = ItemFactory(controller, rest_model)
+
 json_args_post = {
     "name": fields.Str(validate=validate.Length(min=1), required=True),
     "background": fields.Str(validate=lambda v: len(v) == 6, required=False),
     "color": fields.Str(validate=lambda v: len(v) == 6, required=False)
-}
-
-
-@api.route("/")
-class Tags(Resource, Collection):
-    @use_kwargs(Collection.query_args, locations=("query",))
-    def get(self, **kwargs):
-        @api.marshal_with(tag, mask=Collection.mask(kwargs))
-        def response():
-            return [dict(i) for i in controller().get({})]
-        return response()
-
-    @use_kwargs(json_args_post, locations=("json",))
-    def post(self, **kwargs):
-        @api.marshal_with(tag)
-        def response():
-            return dict(controller().create(kwargs))
-        return response()
-
-
-query_args = {
-    "fields": fields.DelimitedList(fields.Str()),
-    "filter": fields.Str(validate=validate.Length(min=2), missing="id")
 }
 
 json_args_patch = {
@@ -52,25 +31,41 @@ json_args_patch = {
 }
 
 
+@api.route("/")
+@api.response(200, "Request successfully executed")
+@api.response(201, "Object has been successfully created")
+@api.response(422, "Bad formatting in Arguments or bad headers")
+class TagCollection(Resource):
+    @api.param("fields", "Used to filter returned fields by name, Syntax: url?fields=id,name", _in="query")
+    @use_kwargs(collection.query_args, locations=("query",))
+    def get(self, **kwargs):
+        return collection.get(**kwargs)
+
+    @api.param("name", "Visible name of the Tag", required=True, _in="body", example="Frontend")
+    @api.param("background", "Optional: 6 digit hex color value without a #", _in="body", example="ffffff")
+    @api.param("color", "Optional: 6 digit hex color value without a #", _in="body", example="000000")
+    @use_kwargs(json_args_post, locations=("json",))
+    def post(self, **kwargs):
+        return collection.post(**kwargs)
+
+
 @api.route("/<string:tag_id>")
-@api.param("tag_id", "May be any valid identifier for a Tag")
-@api.response(404, "Not a valid identifier")
-class Tag(Resource):
-    @use_kwargs(query_args, locations=("query",))
+@api.param("tag_id", "May be any valid identifier for a Tag", _in="path")
+@api.response(200, "Request successfully executed")
+@api.response(422, "Bad formatting in Arguments or bad headers")
+@api.response(500, "Unknown server error during deletion")
+class TagItem(Resource):
+    @api.param("fields", "Used to filter returned fields by name, Syntax: url?fields=id,name", _in="query")
+    @api.param("filter", "Used to specify the type of tag identifier, Syntax: url?filter=name", _in="query")
+    @use_kwargs(item.query_args, locations=("query",))
     def get(self, tag_id: str, **kwargs):
-        mask = "*" if not kwargs.get("fields") else ",".join(kwargs.get("fields"))
-        filter_by = kwargs.get("filter")
+        return item.get(tag_id, **kwargs)
 
-        @api.marshal_with(tag, mask=mask)
-        def response(identifier: str):
-            return [dict(i) for i in controller().get({filter_by: identifier})]
-        return response(tag_id)
-
+    @api.param("tag_id", "Database ID of the Tag to be modified", _in="path")
     @use_kwargs(json_args_patch, locations=("json",))
     def patch(self, tag_id: str, **kwargs):
-        return dict(controller().update(int(tag_id), kwargs))
+        return item.patch(tag_id, **kwargs)
 
+    @api.param("tag_id", "Database ID of the Tag to be deleted", _in="path")
     def delete(self, tag_id: str):
-        operation = controller().delete(int(tag_id))
-        return 200 if operation else 500
-
+        return item.delete(tag_id)
