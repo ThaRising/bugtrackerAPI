c80d39181a58229759cde1b17e025c2c70693542
ThaRising <kochbe.ber@gmail.com>
2020-03-06 15:47:36 +0100

    Continued boilerplate refactoring and improved documentation

--- application/views/issue.py
+++ application/views/issue.py
@@ -5,6 +5,7 @@ from webargs.flaskparser import use_kwargs
 from application.controllers import IssueController
 from .comment import rest_model as comment
 from . import CollectionFactory, ItemFactory
+from application.models import data_models
 
 api = Namespace("issues", description="Issue Resource Endpoint")
 controller = IssueController
@@ -45,19 +46,49 @@ json_args_patch = {
 
 
 @api.route("/")
-@api.response(200, "Request successfully executed")
-@api.response(201, "Object has been successfully created")
 @api.response(422, "Bad formatting in Arguments or bad headers")
 class IssueCollection(Resource):
+    @api.response(200, "Request successfully executed")
     @api.param("fields", "Used to filter returned fields by name, Syntax: url?fields=id,name", _in="query")
     @use_kwargs(collection.query_args, locations=("query",))
     def get(self, **kwargs):
         return collection.get(**kwargs)
 
+    @api.response(201, "Object has been successfully created")
     @api.param("title", "Visible title of the Issue", required=True, _in="body", example="Pls do this!")
     @api.param("reporter", "Database ID of the reporting user", required=True, _in="body", example=1)
     @api.param("assignee", "Database ID of the assigned user", _in="body", example=2)
     @api.param("description", "Issue description", _in="body", example="Hey X, please do this!")
+    @api.param("type", "Database ID of the issue type", _in="body", example=2, default=1, enum=data_models.TYPE)
+    @api.param("status",
+               "Integer corresponding to the issues status", _in="body", example=2, default=1, enum=data_models.STATUS)
+    @api.param("priority", "Integer corresponding to the issues priority level",
+               _in="body", example=2, default=1, enum=data_models.PRIORITY)
     @use_kwargs(json_args_post, locations=("json",))
     def post(self, **kwargs):
         return collection.post(**kwargs)
+
+
+@api.route("/<string:issue_id>")
+@api.param("issue_id", "May be any valid identifier for an Issue", _in="path")
+@api.response(200, "Request successfully executed")
+@api.response(422, "Bad formatting in Arguments or bad headers")
+@api.response(500, "Unknown server error during deletion")
+class TagItem(Resource):
+    @api.param("fields", "Used to filter returned fields by name, Syntax: url?fields=id,name", _in="query")
+    @api.param("filter", "Used to specify the type of tag identifier, Syntax: url?filter=name", _in="query")
+    @use_kwargs(item.query_args, locations=("query",))
+    def get(self, issue_id: str, **kwargs):
+        return item.get(issue_id, **kwargs)
+
+    @api.param("issue_id", "Database ID of the Issue to be modified", _in="path")
+    @api.param("name", "Visible name of the Tag", _in="body", example="Updated Frontend")
+    @api.param("background", "Tag background color, 6 digit hex color value without a #", _in="body", example="212121")
+    @api.param("color", "Tag foreground color, 6 digit hex color value without a #", _in="body", example="f0f0f0")
+    @use_kwargs(json_args_patch, locations=("json",))
+    def patch(self, issue_id: str, **kwargs):
+        return item.patch(issue_id, **kwargs)
+
+    @api.param("issue_id", "Database ID of the Issue to be deleted", _in="path")
+    def delete(self, issue_id: str):
+        return item.delete(issue_id)
